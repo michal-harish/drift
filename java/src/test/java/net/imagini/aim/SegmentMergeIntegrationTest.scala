5237eb76886c0f5f3d79f9824df147525aef2288
Michal Harish <michal.haris@imagini.net>
2014-10-20 14:11:16 +0100

    NOJIRA Filter.fromString and Tokenizer tool

---
+++ java/src/test/java/net/imagini/aim/SegmentMergeIntegrationTest.scala
@@ -0,0 +1,12 @@
+package net.imagini.aim
+
+import org.scalatest.Matchers
+import org.scalatest.FlatSpec
+
+class SegmentMergeIntegrationTest extends FlatSpec with Matchers {
+  
+  "2 sorted segments" should "yield correct groups" in {
+    
+  }
+
+}
