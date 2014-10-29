28fdc9e5d30923d21efa97a750d45bdc5a99bcd7
Michal Harish <michal.haris@imagini.net>
2014-10-29 12:43:10 +0000

    Usecase1 integration test

--- scala/src/test/scala/net/imagini/aim/Usecase1RetroTrainingSet.scala
+++ scala/src/test/scala/net/imagini/aim/Usecase1RetroTrainingSet.scala
@@ -18,53 +18,52 @@ class Usecase1RetroTrainingSet extends FlatSpec with Matchers {
 
   "Usecase1-retroactive measured set " should "should filter one partition by co-partition from another table supllying flags" in {
 
-    //PAGEVIEWS
-    val schemaA = AimSchema.fromString("user_uid(UUID:BYTEARRAY[16]),url(STRING),timestamp(TIME:LONG)")
-    //TODO ttl = 10
-    val sA1 = new AimSegmentQuickSort(schemaA, classOf[BlockStorageLZ4])
+    //PAGEVIEWS //TODO ttl = 10
+    val schemaPageviews = AimSchema.fromString("user_uid(UUID:BYTEARRAY[16]),url(STRING),timestamp(TIME:LONG)")
+    val sA1 = new AimSegmentQuickSort(schemaPageviews, classOf[BlockStorageLZ4])
     sA1.appendRecord("37b22cfb-a29e-42c3-a3d9-12d32850e103", "www.auto.com/mycar", "2014-10-10 11:59:01")
     sA1.appendRecord("37b22cfb-a29e-42c3-a3d9-12d32850e103", "www.travel.com/offers", "2014-10-10 12:01:02")
     sA1.appendRecord("37b22cfb-a29e-42c3-a3d9-12d32850e103", "www.travel.com/offers/holiday", "2014-10-10 12:01:03")
     sA1.appendRecord("12322cfb-a29e-42c3-a3d9-12d32850e103", "www.xyz.com", "2014-10-10 12:01:02")
     sA1.close
-    val sA2 = new AimSegmentQuickSort(schemaA, classOf[BlockStorageLZ4])
+    val sA2 = new AimSegmentQuickSort(schemaPageviews, classOf[BlockStorageLZ4])
     sA2.appendRecord("a7b22cfb-a29e-42c3-a3d9-12d32850e103", "www.bank.com/myaccunt", "2014-10-10 13:59:01")
     sA2.appendRecord("a7b22cfb-a29e-42c3-a3d9-12d32850e103", "www.travel.com/offers", "2014-10-10 13:01:03")
     sA2.close
-    val partitionA1 = new AimPartition(schemaA, 1000)
-    partitionA1.add(sA1)
-    partitionA1.add(sA2)
+    val partitionPageviews1 = new AimPartition(schemaPageviews, 1000)
+    partitionPageviews1.add(sA1)
+    partitionPageviews1.add(sA2)
 
-    //CONVERSIONS
-    val schemaB = AimSchema.fromString("user_uid(UUID:BYTEARRAY[16]),conversion(STRING),url(STRING),timestamp(TIME:LONG)")
-    //TODO ttl = 10
-    val sB1 = new AimSegmentQuickSort(schemaB, classOf[BlockStorageLZ4])
+    //CONVERSIONS //TODO ttl = 10
+    val schemaConversions = AimSchema.fromString("user_uid(UUID:BYTEARRAY[16]),conversion(STRING),url(STRING),timestamp(TIME:LONG)")
+    val sB1 = new AimSegmentQuickSort(schemaConversions, classOf[BlockStorageLZ4])
     sB1.appendRecord("37b22cfb-a29e-42c3-a3d9-12d32850e103", "check", "www.bank.com/myaccunt", "2014-10-10 13:59:01")
     sB1.appendRecord("37b22cfb-a29e-42c3-a3d9-12d32850e103", "buy", "www.travel.com/offers/holiday/book", "2014-10-10 13:01:03")
     sB1.close
-    val partitionB1 = new AimPartition(schemaB, 1000)
-    partitionB1.add(sB1)
+    val partitionConversions1 = new AimPartition(schemaConversions, 1000)
+    partitionConversions1.add(sB1)
 
-    val schemaC = AimSchema.fromString("user_uid(UUID:BYTEARRAY[16]),flag(STRING),value(BOOL)")
-    //TODO ttl = -1
-    val sC1 = new AimSegmentQuickSort(schemaC, classOf[BlockStorageLZ4])
+    //USERFLAGS //TODO ttl = -1
+    val schemaUserFlags = AimSchema.fromString("user_uid(UUID:BYTEARRAY[16]),flag(STRING),value(BOOL)")
+    val sC1 = new AimSegmentQuickSort(schemaUserFlags, classOf[BlockStorageLZ4])
     sC1.appendRecord("37b22cfb-a29e-42c3-a3d9-12d32850e103", "quizzed", "true")
     sC1.appendRecord("37b22cfb-a29e-42c3-a3d9-12d32850e103", "cc", "true")
-    sC1.appendRecord("37b22cfb-a29e-42c3-a3d9-12d32850e103", "opt_out_targetting", "true")
-    sC1.appendRecord("a7b22cfb-a29e-42c3-a3d9-12d32850e103", "cc", "true")
-    sC1.appendRecord("a7b22cfb-a29e-42c3-a3d9-12d32850e103", "quizzed", "false")
     sC1.close
-    val partitionC1 = new AimPartition(schemaC, 1000)
-    partitionC1.add(sC1)
-
-    val eventsJoin = new OuterJoinScanner(
-      new MergeScanner(schemaA, "user_uid,url,timestamp", "url contains 'travel.com'", partitionA1.segments)
-      ,new MergeScanner(schemaB, "user_uid,url,timestamp,conversion", "*", partitionB1.segments)
-    )
+    val sC2 = new AimSegmentQuickSort(schemaUserFlags, classOf[BlockStorageLZ4])
+    sC2.appendRecord("37b22cfb-a29e-42c3-a3d9-12d32850e103", "opt_out_targetting", "true")
+    sC2.appendRecord("a7b22cfb-a29e-42c3-a3d9-12d32850e103", "cc", "true")
+    sC2.appendRecord("a7b22cfb-a29e-42c3-a3d9-12d32850e103", "quizzed", "false")
+    sC2.close
+    val partitionUserFlags1 = new AimPartition(schemaUserFlags, 1000)
+    partitionUserFlags1.add(sC1)
+    partitionUserFlags1.add(sC2)
 
     val tsetJoin = new EquiJoinScanner(
-      new MergeScanner(partitionC1.schema, "user_uid", "value='true' and flag='quizzed' or flag='cc'", partitionC1.segments),
-      eventsJoin
+      new MergeScanner(schemaUserFlags, "user_uid", "value='true' and flag='quizzed' or flag='cc'", partitionUserFlags1.segments),
+      new OuterJoinScanner(
+          new MergeScanner(schemaPageviews, "user_uid,url,timestamp", "url contains 'travel.com'", partitionPageviews1.segments)
+          ,new MergeScanner(schemaConversions, "user_uid,url,timestamp,conversion", "*", partitionConversions1.segments)
+        )
     )
 
     /**
