01177958c84e441ea04f4348f817e618d0e393e6
Michal Harish <michal.haris@imagini.net>
2014-11-19 15:27:19 +0000

    Adding cluster arguments to for true distributed mode

--- scala/src/main/scala/net/imagini/aim/client/DriftCluster.scala
+++ scala/src/main/scala/net/imagini/aim/client/DriftCluster.scala
@@ -10,30 +10,38 @@ import net.imagini.aim.utils.BlockStorageMEMLZ4
 
 object DriftCluster extends App {
   val log = Logger[this.type]
+  var host = "localhost"
   var port: Int = 4000
   var zkConnect: String = "localhost:2181"
-  var clusterId = "default"
+  var clusterId:String = null
   var localNumNodes = 1
+  var firstNode = 1
   val argsIterator = args.iterator
   var storageType: Class[_ <: BlockStorage] = classOf[BlockStorageMEMLZ4]
   while (argsIterator.hasNext) {
     argsIterator.next match {
-      case "--zookeeper" ⇒ zkConnect = argsIterator.next
-      case "--cluster-id"      ⇒ clusterId = argsIterator.next
-      case "--num-nodes" ⇒ localNumNodes = argsIterator.next.toInt
+      case "--zookeeper"    ⇒ zkConnect = argsIterator.next
+      case "--host"         ⇒ host = argsIterator.next
+      case "--cluster-id"   ⇒ clusterId = argsIterator.next
+      case "--first-node"   ⇒ firstNode = argsIterator.next.toInt
+      case "--num-nodes"    ⇒ localNumNodes = argsIterator.next.toInt
       case "--storage-type" ⇒ storageType = Class.forName("net.imagini.aim.utils.BlockStorage" + argsIterator.next).asInstanceOf[Class[BlockStorage]]
-      case "--port"      ⇒ port = argsIterator.next.toInt
-      case arg: String   ⇒ println("Unknown argument " + arg)
+      case "--port"         ⇒ port = argsIterator.next.toInt
+      case arg: String      ⇒ println("Unknown argument " + arg)
     }
+  }
+  if (clusterId == null) {
+    println("Missing --cluster-id argument")
+    System.exit(1);
   }
 
   //SPAWNING CLUSTER
   val manager: DriftManager = new DriftManagerZk(zkConnect, clusterId)
-  println("Joining drift cluster: "+clusterId + "@"+zkConnect)
+  println("Joining drift cluster: " + clusterId + "@" + zkConnect)
   println("Number of local nodes: " + localNumNodes)
   println("Number of total nodes: " + manager.expectedNumNodes)
   println("Storage type: " + storageType.getSimpleName)
-  val localNodes = (1 to localNumNodes).map(n ⇒ new AimNode(n, "localhost:" + (port + n - 1).toString, manager))
+  val localNodes = (1 to localNumNodes).map(n ⇒ new AimNode(n + firstNode - 1, host + ":" + (port + n - 1).toString, manager))
 
   //CREATING TABLES
   //TODO querysession handle CREATE TABLE
@@ -44,7 +52,7 @@ object DriftCluster extends App {
   //  manager.createTable("vdna", "syncs", "user_uid(UUID:BYTEARRAY[16]), timestamp(LONG), id_space(STRING), partner_user_id(STRING)", 100000000, storageType)
 
   //ATTACH CONSOLE
-  val console = new DriftConsole("localhost", 4000)
+  val console = new DriftConsole("localhost", port)
   console.start
 
   //WAIT FOR DISTRIBUTED SHUTDOWN
