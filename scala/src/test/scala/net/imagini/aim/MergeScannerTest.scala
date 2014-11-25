555b83136034f5609d8a61750c4d5389227e200a
Michal Harish <michal.haris@imagini.net>
2014-11-25 17:46:37 +0000

    Bugfix in asynchronous compaction

--- scala/src/test/scala/net/imagini/aim/MergeScannerTest.scala
+++ scala/src/test/scala/net/imagini/aim/MergeScannerTest.scala
@@ -30,6 +30,7 @@ class MergeScannerTest extends FlatSpec with Matchers {
     s2.appendRecord("17b22cfb-a29e-42c3-a3d9-12d32850e103", "pageview", "{www.music.com}")
 
     region.add(s1)
+    region.compact
     region.add(s2)
     region.compact
 
